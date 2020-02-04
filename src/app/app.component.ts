234973ce359c60750fef01a30ef251ea1341f9f1
anvitaaps <anvitaaps23@gmail.com>
2020-02-04 23:30:06 +0530

    Final commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AppService } from './app.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'framesAssignment';
+  url = 'https://reqres.in/api/users?page=2';
+  data;
+  original_data;
+
+  constructor(private appService: AppService) {
+  }
+
+  ngOnInit() {
+    this.appService.get_data(this.url).subscribe((res)=> {
+      console.log("response",res);
+      this.data = res.data;
+      this.original_data = res.data;
+      },
+    (error)=>{
+        console.log(error);
+    });
+  }
+
+  applyFilter(event) {
+    let temp_arr = []
+    this.original_data.map(item => {
+      if (item.email.includes(event))
+        temp_arr.push(item);
+    });
+    this.data = temp_arr;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { MatCardModule, MatFormFieldModule, MatInputModule, MatIconModule } from '@angular/material';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; 
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatCardModule,
+    MatExpansionModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
